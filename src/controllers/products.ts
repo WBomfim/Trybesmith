b177f619edc75b7f0a6d2683ff3a52dd8e2a7439
WBomfim <willianbomfim@hotmail.com>
2022-09-13 22:44:58 -0300

    refactor: Refatorado as funções da camada controllers

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,18 +1,15 @@
 import { Request, Response } from 'express';
 import * as productsService from '../services/products';
 
-const createProduct = async (req: Request, res: Response): Promise<Response> => {
+export const createProduct = async (req: Request, res: Response): Promise<Response> => {
   const { name, amount } = req.body;
-  const product = await productsService.createProduct({ name, amount });
-  return res.status(product.code).json(product.data);
+  const { code, data, error } = await productsService.createProduct({ name, amount });
+  if (error) return res.status(code).json(error);
+  return res.status(code).json(data);
 };
 
-const getAllProducts = async (_req: Request, res: Response): Promise<Response> => {
-  const products = await productsService.getAllProducts();
-  return res.status(products.code).json(products.data);
-};
-
-export {
-  createProduct,
-  getAllProducts,
+export const getAllProducts = async (_req: Request, res: Response): Promise<Response> => {
+  const { code, data, error } = await productsService.getAllProducts();
+  if (error) return res.status(code).json(error);
+  return res.status(code).json(data);
 };
