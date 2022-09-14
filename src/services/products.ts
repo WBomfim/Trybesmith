4cbc4d3e07d5dac75d2505e2f6870ad2333555e9
WBomfim <willianbomfim@hotmail.com>
2022-09-13 23:15:41 -0300

    feat: Criado um enum para os status Http

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,16 +1,27 @@
 import * as productModel from '../models/product';
 import { IReturnProducts, IReturnProduct } from '../interfaces/returnService';
 import Product from '../interfaces/product';
+import StatusHttp from '../types/statusHttp';
 
 export const createProduct = async (product: Product): Promise<IReturnProduct> => {
   const { name, amount } = product;
   const newProduct = await productModel.createProduct({ name, amount });
-  if (!newProduct) return { code: 501, error: { message: 'Product not created' } };
-  return { code: 201, data: newProduct };
+  if (!newProduct) {
+    return { 
+      code: StatusHttp.NOT_IMPLEMENTED,
+      error: { message: 'Product not created' },
+    };
+  }
+  return { code: StatusHttp.OK, data: newProduct };
 };
 
 export const getAllProducts = async (): Promise<IReturnProducts> => {
   const products = await productModel.getAllProducts();
-  if (!products) return { code: 404, error: { message: 'Products not found' } };
-  return { code: 200, data: products };
+  if (!products) {
+    return { 
+      code: StatusHttp.NOT_FOUND, 
+      error: { message: 'Products not found' },
+    };
+  }
+  return { code: StatusHttp.OK, data: products };
 };
---
+++ src/types/statusHttp.ts
@@ -0,0 +1,12 @@
+enum StatusHttp {
+  OK = 200,
+  CREATED = 201,
+  NO_CONTENT = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  INTERNAL_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+}
+
+export default StatusHttp;
