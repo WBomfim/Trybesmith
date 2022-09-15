7206e088a4219355e281c9db917a8f82ee11e8d3
WBomfim <willianbomfim@hotmail.com>
2022-09-15 04:18:39 -0300

    feat: Criado o endpoint POST /orders

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import orderController from '../controllers/orders';
+import auth from '../middlewares/auth';
 
 const router = Router();
 
 router.get('/orders', orderController.getAllOrders);
+
+router.post('/orders', auth, orderController.createOrder);
 
 export default router;
